d25652af2405f93f5183527cb1e6c52f77692531
Trevor Lane <tlane@simplenexus.com>
2020-01-28 13:03:51 -0700

    adds loud mode and sudo car mode

--- sound_testing_stuffs.cpp
+++ sound_testing_stuffs.cpp
@@ -1,25 +1,56 @@
-
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
 
+
+
 BLEServer* pServer = NULL;
 BLECharacteristic* pCharacteristic = NULL;
+BLECharacteristic* volumeCharacteristic = NULL;
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
-uint32_t value = 0;
-int counter = 0;
+
+
+int loudCounter = 0;
 int digitalValue = 0;
-int pin = A0;
+int soundInput = A0;
+int motorA = A1;
+int motorB = A6;
 
+byte value = 0;
+int vibratingState = LOW;
 
-// See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
 
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#define volumenCharacteristic_UUID "088a1e2a-1991-411e-8a73-f71bdcd7487c"
 
+#define offState 1
+#define onState 2
+#define carState 3
+#define sootheState 4
+#define loudState 5
+
+int currentState = 1;
+
+
+class MyCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *volumeCharacteristic) {
+      std::string value = volumeCharacteristic->getValue();
+
+      if (value.length() > 0) {
+//        Serial.println("*********");
+          int strVal = value[0] - '0';
+          currentState = strVal;
+          Serial.print("strVal: ");   
+          Serial.println(strVal); 
+//        Serial.println();
+//        Serial.println("*********");
+      }
+    }
+};
 
 class MyServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
@@ -32,13 +63,9 @@ class MyServerCallbacks: public BLEServerCallbacks {
 };
 
 
-
 void setup() {
   
   Serial.begin(115200);
-
-
-  counter = 0;
   BLEDevice::init("ESP32");
 
   // Create the BLE Server
@@ -56,64 +83,141 @@ void setup() {
                       BLECharacteristic::PROPERTY_NOTIFY |
                       BLECharacteristic::PROPERTY_INDICATE
                     );
-
-  // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
-  // Create a BLE Descriptor
+                    
+//  pCharacteristic->setCallbacks(new MyCallbacks());
   pCharacteristic->addDescriptor(new BLE2902());
 
-  // Start the service
+  volumeCharacteristic = pService->createCharacteristic(
+                    volumenCharacteristic_UUID,
+                    BLECharacteristic::PROPERTY_READ   |
+                    BLECharacteristic::PROPERTY_WRITE  |
+                    BLECharacteristic::PROPERTY_NOTIFY |
+                    BLECharacteristic::PROPERTY_INDICATE
+                  );
+
+  volumeCharacteristic->setCallbacks(new MyCallbacks());
+  volumeCharacteristic->addDescriptor(new BLE2902());
+ 
   pService->start();
 
   // Start advertising
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->setScanResponse(false);
-  pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
+  pAdvertising->setMinPreferred(0x0); 
   BLEDevice::startAdvertising();
 
   
   Serial.println("Waiting a client connection to notify...");
-  pinMode (pin, INPUT); // Set the signal pin as input   
+ 
   pinMode(LED_BUILTIN, OUTPUT);
-
+  pinMode (soundInput, INPUT);
+  pinMode(motorA, OUTPUT);
+  pinMode(motorB, OUTPUT);
 }
 
-void loop() {
+void triggerOffState(){
+  digitalWrite(motorA, LOW);   
+  digitalWrite(motorB, LOW);
+  loudCounter = -5;
+}
 
-    // notify changed value
+void triggerOnState(){
+  digitalWrite(motorA, HIGH);   
+  digitalWrite(motorB, HIGH);
+}
+void triggerSootheState(){
+  digitalWrite(motorA, LOW);  
+  digitalWrite(motorB, HIGH);
+  delay(500);      
+  digitalWrite(motorA, HIGH); 
+  digitalWrite(motorB, LOW); 
+  delay(500);   
+}
+void triggerLoudState(){
+   Serial.print("loudCounter: ");
+   Serial.println(loudCounter);
+             
+  if(loudCounter > 0){
+   digitalWrite(motorA, HIGH);  
+   digitalWrite(motorB, HIGH);
+  } else {
+   digitalWrite(motorA, LOW);  
+   digitalWrite(motorB, LOW);
+  }
+
+  delay(1000);      
+}
+
+
+
+void loop() {
+  
     if (deviceConnected) {
 
-       Serial.println("connected");
+      
+      switch (currentState) {
+        case 1:
+         triggerOffState();
+          break;
+        case 2:
+          triggerOnState();
+          break;
+        case 3:
+          triggerOnState();
+          break;
+        case 4:
+          triggerSootheState();
+          break;
+        case 5:
+          triggerLoudState();
+          break;
+        default:
+          triggerOffState();
+          break;
+      }
 
- 
+      
         digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-        delay(250);                       // wait for a second
+        delay(100);                       // wait for a second
         digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-        delay(250);   
+        delay(100);   
+   
+//        digitalWrite(motorA, vibratingState);   // turn the LED on (HIGH is the voltage level)
+//        digitalWrite(motorB, vibratingState);
+       
 
+        digitalValue = digitalRead(soundInput);
 
+        if(digitalValue){
+           
+            
+             if(loudCounter > -5){
+                loudCounter -= 1;
+             }
 
-        counter++;
-        digitalValue = digitalRead(pin);
+//             Serial.println("quiet");
 
-          if(digitalValue){
-              Serial.println("quiet");
-               pCharacteristic->setValue("Quiet");
-               pCharacteristic->notify();  
-            } else {
-               Serial.println("LOUD!!!");
-               pCharacteristic->setValue("Loud");
-               pCharacteristic->notify();  
-            }
+             
+             pCharacteristic->setValue("0");
+             pCharacteristic->notify();  
+          } else {
+            
+             loudCounter = 25;
+//             Serial.println("LOUD!!!");
+//             Serial.print("loudCounter: ");
+//             Serial.println(loudCounter);
+            
+             pCharacteristic->setValue("1");
+             pCharacteristic->notify();  
+          }
 
-//        pCharacteristic->setValue(analogValue);
-//        pCharacteristic->notify();
-        value++;
-        delay(100); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
+//        delay(200); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
     }
     // disconnecting
     if (!deviceConnected && oldDeviceConnected) {
         delay(500); // give the bluetooth stack the chance to get things ready
+        triggerOffState();
         pServer->startAdvertising(); // restart advertising
         Serial.println("start advertising");
         oldDeviceConnected = deviceConnected;
